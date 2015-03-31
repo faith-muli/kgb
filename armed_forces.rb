43965efd034f195ce7e24a502a1375a95ada7016
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-03-31 11:47:24 +0300

    Privatesizes strategize method in GuildMaster class and restricts access to children

---
+++ armed_forces.rb
@@ -0,0 +1,7 @@
+require_relative 'guild_master'
+
+class ArmedForces < GuildMaster
+end
+
+p ian = ArmedForces.new('Ian', 'AF')
+ian.strategize
--- guild_master.rb
+++ guild_master.rb
@@ -3,4 +3,11 @@ class GuildMaster
     @name = name
     @rank = rank
   end
+
+  private
+    def strategize
+      puts "I am strategizing"
+    end
 end
+
+p faith = GuildMaster.new('Faith', 'GM')
